c784d290f106baec2414dad6724bdbee08cd8cd6
SonTrungTo <son.trung.to@gmail.com>
2022-04-17 08:25:57 +0300

    ch2 done!

--- ch2-sample-project/app/Http/Controllers/Controller.php
+++ ch2-sample-project/app/Http/Controllers/Controller.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+$bugsnag = new Bugsnag(config('services.bugsnag.api_key'));
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
